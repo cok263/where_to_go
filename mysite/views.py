8d75179175ccd89db26a0ffa27c9b385fde47e24
cok263 <cok26385@gmail.com>
2020-12-23 15:09:42 +0300

    Add frontend

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,7 @@ from django.template import loader
 
 
 def show_where_to_go(request):
-    template = loader.get_template('product_page.html')
+    template = loader.get_template('index.html')
     context = {}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)

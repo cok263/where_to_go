d4861bbadfbface805ed6d2e86badee2d8b1d09c
cok263 <cok26385@gmail.com>
2020-12-25 13:59:56 +0300

    Move data from template to view

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,42 @@ from django.template import loader
 
 
 def show_where_to_go(request):
+
+    places = {
+	    "type": "FeatureCollection",
+	    "features": [
+	    	{
+          		"type": "Feature",
+          		"geometry": {
+           			"type": "Point",
+            		"coordinates": [37.62, 55.793676]
+          		},
+          		"properties": {
+            	"title": "«Легенды Москвы",
+            	"placeId": "moscow_legends",
+            	"detailsUrl": "./places/moscow_legends.json"
+          		}
+        	},
+        	{
+          		"type": "Feature",
+          		"geometry": {
+            		"type": "Point",
+            		"coordinates": [37.64, 55.753676]
+          		},
+          		"properties": {
+            		"title": "Крыши24.рф",
+            		"placeId": "roofs24",
+            		"detailsUrl": "./places/roofs24.json"
+          		}
+        	}
+      	]
+    }
+
+    data = {
+    	"places": places,
+    }
+
     template = loader.get_template('index.html')
-    context = {}
+    context = data
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
